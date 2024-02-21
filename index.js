f0d0573259af767c7ba3e85f5a800ec94ae37642
Esen Zhanyshov <e.zhanyshov@gmail.com>
2024-02-21 12:41:30 +0600

    fatal

---
+++ index.js
@@ -0,0 +1,92 @@
+import express from "express";
+import multer from "multer";
+import cors from "cors";
+
+import mongoose from "mongoose";
+
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
+
+import checkAuth from "./utils/checkAuth.js";
+import handleValidationErrors from "./utils/handleValidationErrors.js";
+
+import { UserController, PostController } from "./controllers/index.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:wwwwww@cluster0.wz8twgm.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use("/uploads/", express.static("uploads"));
+
+// ! login
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
+// ! register
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+// ! get
+app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+// ! posts
+
+app.get("/tags", PostController.getLastTags);
+
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.post(
+  "/posts",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.create
+);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch(
+  "/posts/:id",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update
+);
+
+app.listen(3333, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Server OK");
+});
